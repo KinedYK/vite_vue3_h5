344d00bbe4afedb04062578554ad68ff8cc2b0e5
KinedYK <yin.kk@qq.com>
2022-06-16 13:17:44 +0800

    同步

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'XXX',
-        changeOrigin: true,
+        changeOrigin: true, // 是否启用host
         // rewrite: (path) => path.replace(/^\/api/, '')
       },
     }
