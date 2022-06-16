4de09941f83cfbcda71ff5e8a597418751329642
KinedYK <yin.kk@qq.com>
2022-06-16 14:41:40 +0800

    设备信息

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import VueClipboard from 'vue-clipboard2'
 
-// 设备信息 demo
+/**
+ * link(https://github.com/KinedYK/vite_vue3_h5/blob/master/src/components/HelloWorld.vue)
+ * 设备信息
+ */ 
 import { isMobile, getUA } from 'mobile-device-detect'
 console.log(isMobile)
 console.log(getUA)
