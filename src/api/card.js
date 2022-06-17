b5abf37a9e9638d129602f90004024f851bfa3ce
KinedYK <yin.kk@qq.com>
2022-06-17 11:05:37 +0800

    api

---
+++ src/api/card.js
@@ -0,0 +1,11 @@
+import request from "@/utils/request";
+
+// demo
+// export function articleDetail(params) {
+//   //类型
+//   return request({
+//     url: "/article/detail",
+//     method: "get",
+//     params: params,
+//   });
+// }
